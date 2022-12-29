6b4d12f707097b4c1163b7ac63fbaa04f2aa133e
Enver Yakubov <enveryakubov@gmail.com>
2022-12-29 22:35:12 +0300

    Finished css layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './sass/index.scss';
 
 console.log('Hurray!');
+
+const test = document.querySelector('.excel__formula .input');
+
+test.addEventListener('input', (e) => console.log(e.target.innerText));
